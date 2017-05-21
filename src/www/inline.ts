2606a78a9a3591abc753c880ce87f7980d66fa66
Douglas Duteil <douglasduteil@gmail.com>
2017-05-21 20:27:02 +0200

    feat(inline): add the script after dom ready

--- src/www/inline.ts
+++ src/www/inline.ts
@@ -2,10 +2,12 @@
 
 import "./base.scss";
 
+const fistScriptEl = document.getElementsByTagName("script")[0];
 const webfontScriptEl = document.createElement("script");
 webfontScriptEl.src = "https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js";
 webfontScriptEl.type = "text/javascript";
 webfontScriptEl.async = true;
 
-const fistScriptEl = document.getElementsByTagName("script")[0];
-fistScriptEl.parentNode.insertBefore(webfontScriptEl, fistScriptEl);
+document.addEventListener("DOMContentLoaded", function launchLongExecution() {
+  fistScriptEl.parentNode.insertBefore(webfontScriptEl, fistScriptEl);
+});
